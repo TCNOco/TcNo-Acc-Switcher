950395d8c91f740bcb65858d2cd66cb456da6bc9
Wesley Pyburn (TechNobo) <technobo@tcno.co>
2019-11-22 16:18:13 +0200

    Init
    Some basic design work complete with C# Core 3.0 WPF. Still super basic, and in no way functional just yet.

---
+++ TCNO-Acc-Switcher-CSharp-WPF/TCNO-Acc-Switcher-CSharp-WPF/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TCNO_Acc_Switcher_CSharp_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ TCNO-Acc-Switcher-CSharp-WPF/TCNO-Acc-Switcher-CSharp-WPF/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TCNO_Acc_Switcher_CSharp_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button loginbutton = sender as Button;
+
+            ColorAnimation animation;
+            animation = new ColorAnimation();
+            animation.From = Colors.Orange;
+            animation.To = (Color)(ColorConverter.ConvertFromString("#0c0c0c"));
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(5));
+
+            loginbutton.Background = new SolidColorBrush(Colors.Orange);
+            loginbutton.Background.BeginAnimation(SolidColorBrush.ColorProperty, animation);
+        }
+    }
+}
