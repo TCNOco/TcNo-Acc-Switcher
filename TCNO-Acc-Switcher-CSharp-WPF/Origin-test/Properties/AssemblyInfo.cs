db10efd9a2daf1607a1627f51ca2bb6d7a327e06
Wesley Pyburn (TechNobo) <technobo@tcno.co>
2020-04-23 21:49:01 +0200

    Origin switcher test program
    Successfull account switching test

---
+++ TCNO-Acc-Switcher-CSharp-WPF/Origin-test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Origin-test")]
+[assembly: AssemblyDescription("Test for TcNo Account Switcher - Origin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TechNobo (Wesley Pyburn)")]
+[assembly: AssemblyProduct("TcNo Account Switcher - Origin Test")]
+[assembly: AssemblyCopyright("Copyright © TechNobo (Wesley Pyburn) 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a447e0ac-ec3d-46b1-94c9-3952f2be35ad")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
