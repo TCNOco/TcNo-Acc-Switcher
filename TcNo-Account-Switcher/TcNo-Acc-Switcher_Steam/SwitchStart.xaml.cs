49af0ab28781732627cad294a789deaad472e163
Wesley Pyburn (TechNobo) <technobo@tcno.co>
2020-07-13 18:51:01 +0200

    Switch and Start app
    Started work on selector to switch accounts, then start an app or connect to a server.

---
+++ TcNo-Account-Switcher/TcNo-Acc-Switcher_Steam/SwitchStart.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TcNo_Acc_Switcher_Steam
+{
+    /// <summary>
+    /// Interaction logic for SwitchStart.xaml
+    /// 
+    /// PLAN:
+    /// Dropdown menu with:
+    /// - steam://connect/<IP>
+    /// - steam://rungameid/<ID>
+    /// Textbox for entering gameID or IP.
+    /// - Button to search for game, brings up list with list of games from Steam -- Mentioned in UserDataWindow
+    /// Previous starts
+    /// 
+    /// </summary>
+    public partial class SwitchStart : Window
+    {
+        public SwitchStart()
+        {
+            InitializeComponent();
+        }
+    }
+}
