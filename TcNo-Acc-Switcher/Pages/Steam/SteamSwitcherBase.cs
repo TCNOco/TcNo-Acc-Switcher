f04d71357e1dc3766d0cf9db8735b635d0e8cd48
Wesley Pyburn (TechNobo) <TechNobo@tcno.co>
2020-08-16 15:07:03 +0200

    Account component. Started work on interop

---
+++ TcNo-Acc-Switcher/Pages/Steam/SteamSwitcherBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace TcNo_Acc_Switcher.Pages.Steam
+{
+    public class SteamSwitcherBase
+    {
+        [JSInvokable]
+        public static Task<int> CopyProfileURL()
+        {
+            Console.WriteLine("ffffffffffffffffffffff");
+            return Task.FromResult(0);
+        }
+
+        //[JSInvokable]
+        //public static Task<int> CopyProfileUrl()
+        //{
+        //    return Task.FromResult(new Random().Next());
+        //}
+    }
+}
