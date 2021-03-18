21fcbc53675ae4d917c7c601a239fc9b8c48a861
Wesley Pyburn (TechNobo) <10319195+TcNobo@users.noreply.github.com>
2021-03-19 01:30:02 +0200

    Progress on settings styling

---
+++ TcNo-Acc-Switcher-Server/Pages/Steam/Settings.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TcNo_Acc_Switcher_Server.Pages.Steam
+{
+    public class SettingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
