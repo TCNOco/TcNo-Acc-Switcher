b7b8b8f2962899f4e4c0fde0b19ab7495f42e185
Wesley Pyburn (TechNobo) <10319195+TcNobo@users.noreply.github.com>
2021-03-28 18:06:24 +0200

    Steam settings save/load.
    The settings page now has persistent settings, even though they don't do anything just yet.
    Huge issue though: Previously (being Steam only) there was 1 settings file, now there's going to be 1 per program. This is fine, but it needs to be an object rather than a set-in-stone class. This is fine, just a lot of work to swap out.
    
    A file for program settings (Last size etc).
    A file for each switcher in the account switcher.

---
+++ TcNo-Acc-Switcher-Server/Pages/General/GeneralInvocableFuncs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TcNo_Acc_Switcher_Server.Pages.General
+{
+    public class GeneralInvocableFuncs
+    {
+        [JSInvokable]
+        public static async void GISaveSettings(string file, string jsonString)
+        {
+            string sFilename = file + ".json";
+
+            // Get existing settings
+            JObject joSettings = new JObject();
+            try
+            {
+                joSettings = JObject.Parse(await File.ReadAllTextAsync(sFilename));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            JObject joNewSettings = JObject.Parse(jsonString);
+
+            // Merge existing settings with settings from site
+            joSettings.Merge(joNewSettings, new JsonMergeSettings
+            {
+                MergeArrayHandling = MergeArrayHandling.Union
+            });
+
+            // Save all settings back into file
+            await File.WriteAllTextAsync(sFilename, joSettings.ToString());
+        }
+
+        [JSInvokable]
+        public static Task GILoadSettings(string file)
+        {
+            string sFilename = file + ".json";
+            string output = "";
+            try
+            {
+                output = File.ReadAllText(sFilename);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return Task.FromResult(output);
+        }
+    }
+}
---
+++ TcNo-Acc-Switcher-Server/wwwroot/js/steam/settings.js
@@ -0,0 +1,40 @@
+﻿// Set settings
+function setSettings(settingsJsonString) {
+    var settingsJson = JSON.parse(settingsJsonString);
+    $('input[type="checkbox"]').each(function () { // Iterate over checkboxes
+        $(this).prop("checked", settingsJson[$(this).prop("id")]);
+    });
+
+    $('input[type="number"]').each(function () { // Iterate over number inputs
+        $(this).val(settingsJson[$(this).prop("id")]);
+    });
+}
+// Load settings
+export function jsLoadSettings() {
+    DotNet.invokeMethodAsync("TcNo-Acc-Switcher-Server", "GILoadSettings", "SteamSettings").then(r => {
+        console.log("GOT SETTINGS");
+        console.log(r);
+        setSettings(r);
+    });
+    console.log("Getting settings from file");
+}
+
+
+
+// Get settings
+function getSettings() {
+    var settingsJson = {};
+    $('input[type="checkbox"]').each(function () { // Iterate over checkboxes
+        settingsJson[$(this).prop("id")] = $(this).prop("checked");
+    });
+
+    $('input[type="number"]').each(function () { // Iterate over number inputs
+        settingsJson[$(this).prop("id")] = $(this).val();
+    });
+    return JSON.stringify(settingsJson);
+}
+// Save settings
+export function jsSaveSettings() {
+    DotNet.invokeMethodAsync("TcNo-Acc-Switcher-Server", "GISaveSettings", "SteamSettings", getSettings());
+}
+
