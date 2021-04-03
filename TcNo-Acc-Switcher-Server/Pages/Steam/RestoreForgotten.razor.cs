65b0531870c1edb7df775ec596d58a79a45cc444
Wesley Pyburn (TechNobo) <10319195+TcNobo@users.noreply.github.com>
2021-04-03 12:29:13 +0200

    Forgotten accounts restore
    Started work towards restoring forgotten accounts.
    Want to include the ability to do multiple at once, this might be difficult though.

---
+++ TcNo-Acc-Switcher-Server/Pages/Steam/RestoreForgotten.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace TcNo_Acc_Switcher_Server.Pages.Steam
+{
+    public partial class RestoreForgotten : ComponentBase
+    {
+    }
+}
