18781dab244920146d3bbfff854ba0c21f0cb2f9
Wesley Pyburn (TechNobo) <10319195+TcNobo@users.noreply.github.com>
2021-04-19 19:50:13 +0200

    Fixed client not opening.
    Even though 2 DLL files aren't used, they are somehow linked to the main program. This should fix the issue.

--- TcNo-Acc-Switcher-Client/PostBuild.bat
+++ TcNo-Acc-Switcher-Client/PostBuild.bat
@@ -6,14 +6,14 @@ cd bin\x64\Debug\net5.0-windows\
 mkdir updater
 mkdir updater\x64
 mkdir updater\x86
-move /Y "VCDiff.dll" "updater\VCDiff.dll"
+copy /B /Y "VCDiff.dll" "updater\VCDiff.dll"
 move /Y "TcNo-Acc-Switcher-Updater.runtimeconfig.json" "updater\TcNo-Acc-Switcher-Updater.runtimeconfig.json"
 move /Y "TcNo-Acc-Switcher-Updater.runtimeconfig.dev.json" "updater\TcNo-Acc-Switcher-Updater.runtimeconfig.dev.json"
 move /Y "TcNo-Acc-Switcher-Updater.pdb" "updater\TcNo-Acc-Switcher-Updater.pdb"
 move /Y "TcNo-Acc-Switcher-Updater.exe" "updater\TcNo-Acc-Switcher-Updater.exe"
 copy /B /Y "TcNo-Acc-Switcher-Updater.dll" "updater\TcNo-Acc-Switcher-Updater.dll"
 move /Y "TcNo-Acc-Switcher-Updater.deps.json" "updater\TcNo-Acc-Switcher-Updater.deps.json"
-move /Y "SevenZipExtractor.dll" "updater\SevenZipExtractor.dll"
+copy /B /Y "SevenZipExtractor.dll" "updater\SevenZipExtractor.dll"
 move /Y "x86\7z.dll" "updater\x86\7z.dll"
 move /Y "x64\7z.dll" "updater\x64\7z.dll"
 copy /B /Y "ref\TcNo-Acc-Switcher-Updater.dll" "updater\ref\TcNo-Acc-Switcher-Updater.dll"
@@ -32,14 +32,14 @@ cd ..\..\Release\net5.0-windows\
 mkdir updater
 mkdir updater\x64
 mkdir updater\x86
-move /Y "VCDiff.dll" "updater\VCDiff.dll"
+copy /B /Y "VCDiff.dll" "updater\VCDiff.dll"
 move /Y "TcNo-Acc-Switcher-Updater.runtimeconfig.json" "updater\TcNo-Acc-Switcher-Updater.runtimeconfig.json"
 move /Y "TcNo-Acc-Switcher-Updater.runtimeconfig.dev.json" "updater\TcNo-Acc-Switcher-Updater.runtimeconfig.dev.json"
 move /Y "TcNo-Acc-Switcher-Updater.pdb" "updater\TcNo-Acc-Switcher-Updater.pdb"
 move /Y "TcNo-Acc-Switcher-Updater.exe" "updater\TcNo-Acc-Switcher-Updater.exe"
 copy /B /Y "TcNo-Acc-Switcher-Updater.dll" "updater\TcNo-Acc-Switcher-Updater.dll"
 move /Y "TcNo-Acc-Switcher-Updater.deps.json" "updater\TcNo-Acc-Switcher-Updater.deps.json"
-move /Y "SevenZipExtractor.dll" "updater\SevenZipExtractor.dll"
+copy /B /Y "SevenZipExtractor.dll" "updater\SevenZipExtractor.dll"
 move /Y "x86\7z.dll" "updater\x86\7z.dll"
 move /Y "x64\7z.dll" "updater\x64\7z.dll"
 copy /B /Y "ref\TcNo-Acc-Switcher-Updater.dll" "updater\ref\TcNo-Acc-Switcher-Updater.dll"
