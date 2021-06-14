c10b9462bdf8ec8ff704bed8e37c8abe345fcdc5
Wesley Pyburn (TechNobo) <10319195+TcNobo@users.noreply.github.com>
2021-06-14 02:52:22 +0200

    Fixed Newtonsoft json error
    - FINALLY fixed annoying Newtonsoft.json error. Had no idea how to catch it when it wasn't breaking in Visual Studio... Turned on ALL exceptions and managed to catch, and handle it.
    - Enabled language dropdown for testing... Still needs LOTS of work, even before translating

--- TcNo-Acc-Switcher-Server/Shared/ContextMenu.razor.cs
+++ TcNo-Acc-Switcher-Server/Shared/ContextMenu.razor.cs
@@ -43,49 +43,45 @@ namespace TcNo_Acc_Switcher_Server.Shared
         {
             var j = JToken.FromObject(o);
             // See if it's a string
-            try
-            {
-                var action = j.Value<string>();
-                // Add key and string item
-                _htmlOut += $"<li><a onclick=\"{action}\">{s}</a></li>\n";
-                return;
-            }
-            catch (InvalidCastException)
-            {
-                // Left blank
-            }
-            catch(Exception e)
-            {
-                Globals.WriteToLog(e.ToString());
-            }
+            if (j.Type == JTokenType.String)
+                try
+	            {
+	                var action = j.Value<string>();
+	                // Add key and string item
+	                _htmlOut += $"<li><a onclick=\"{action}\">{s}</a></li>\n";
+	            }
+	            catch(Exception e)
+	            {
+	                Globals.WriteToLog(e.ToString());
+	            }
 
             // See if it's an array
-            try
-            {
-                // Add key and string item
-                var jArray = j.Value<JArray>();
-                _htmlOut += $"<li><a onclick=\"event.preventDefault();\">{s}</a>\n\t<ul class=\"submenu\">";
-                // Foreach
-                foreach (var jToken in jArray)
-                {
-                    // Each item
-                    foreach (var (key, value) in JObject.FromObject(jToken))
-                    {
-                        if (key == "")
-                            _htmlOut = GeneralFuncs.ReplaceLast(_htmlOut, "event.preventDefault();", value.Value<string>()); // Replace last occurence
-                        else ProcessContextItem(key, value);
-                    }
-                }
-                _htmlOut += "\t</ul>\n</li>";
-            }
-            catch (InvalidCastException)
-            {
-                // Left blank
-            }
-            catch (Exception e)
-            {
-                Globals.WriteToLog(e.ToString());
-            }
+            else if (j.Type == JTokenType.Array)
+                try
+	            {
+	                // Add key and string item
+	                var jArray = j.Value<JArray>();
+	                if (jArray == null) return;
+                    _htmlOut += $"<li><a onclick=\"event.preventDefault();\">{s}</a>\n\t<ul class=\"submenu\">";
+	                // Foreach
+	                foreach (var jToken in jArray)
+	                {
+		                // Each item
+		                foreach (var (key, value) in JObject.FromObject(jToken))
+		                {
+			                if (key == "")
+				                _htmlOut = GeneralFuncs.ReplaceLast(_htmlOut, "event.preventDefault();",
+					                value.Value<string>()); // Replace last occurrence
+			                else ProcessContextItem(key, value);
+		                }
+	                }
+
+	                _htmlOut += "\t</ul>\n</li>";
+	            }
+	            catch (Exception e)
+	            {
+	                Globals.WriteToLog(e.ToString());
+	            }
         }
     }
 }
