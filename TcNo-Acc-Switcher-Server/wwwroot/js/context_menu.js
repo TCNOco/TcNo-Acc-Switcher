3f2ca6c1354924cb547d6cd5cbf8f27fd7bf0175
Wesley Pyburn (TechNobo) <10319195+TcNobo@users.noreply.github.com>
2021-06-27 14:37:29 +0200

    Context menu position changes
    - Adjusted where the context menu opens, and expands.

--- TcNo-Acc-Switcher-Server/wwwroot/js/context_menu.js
+++ TcNo-Acc-Switcher-Server/wwwroot/js/context_menu.js
@@ -62,28 +62,17 @@ function initContextMenu() {
         //Get contextmenu size:
         const menuWidth = $(".contextmenu").width();
         const menuHeight = $(".contextmenu").height();
-        //Security margin:
-        const secMargin = 10;
-        //Prevent page overflow:
-        var posLeft, posTop;
-        if (posX + menuWidth + secMargin >= winWidth &&
-            posY + menuHeight + secMargin >= winHeight) {
-            //Case 1: right-bottom overflow:
-            posLeft = posX - menuWidth - secMargin + "px";
-            posTop = posY - menuHeight - secMargin + "px";
-        } else if (posX + menuWidth + secMargin >= winWidth) {
-            //Case 2: right overflow:
-            posLeft = posX - menuWidth - secMargin + "px";
-            posTop = posY + secMargin + "px";
-        } else if (posY + menuHeight + secMargin >= winHeight) {
-            //Case 3: bottom overflow:
-            posLeft = posX + secMargin + "px";
-            posTop = posY - menuHeight - secMargin + "px";
-        } else {
-            //Case 4: default values:
-            posLeft = posX + secMargin + "px";
-            posTop = posY + secMargin + "px";
-        }
+
+        // Header offset + 10:
+        const hOffset = 42;
+        // Prevent page overflow:
+        const xOverflow = posX + menuWidth + hOffset - winWidth;
+        const yOverflow = posY + menuHeight + hOffset - winHeight;
+        console.log("X: " + xOverflow + "Y: " + yOverflow);
+
+        var posLeft = posX + (xOverflow > 0 ? - menuWidth : 10) + "px";
+        var posTop = posY + (yOverflow > 0 ? - yOverflow : 10) + "px";
+
         //Display contextmenu:
         $(".contextmenu").css({
             "left": posLeft,
@@ -96,22 +85,34 @@ function initContextMenu() {
     // Check element fits on page, and move if it doesn't
     // This function moves the element to the left if it doesn't fit.
     var contextMenu = document.getElementsByClassName("contextmenu")[0];
-    var rightSpace = 0;
+    var rightSpace = 0, bottomSpace = 0;
 
-    function moveContextMenu(submenuSize) {
-        rightSpace = $(contextMenu).position().left + $(contextMenu).width() + submenuSize - window.innerWidth;
+    function moveContextMenu(submenu) {
+        //rightSpace = $(contextMenu).position().left + $(contextMenu).width() + submenuSize - window.innerWidth;
+        rightSpace = $(submenu).offset().left + $(submenu).width() - window.innerWidth;
+        console.log($(submenu).offset().left + " + " + $(submenu).width() + " - " + window.innerWidth + " = " + rightSpace);
+
+        bottomSpace = $(submenu).offset().top + $(submenu).height() - window.innerHeight;
+        console.log($(submenu).offset().top + " + " + $(submenu).height() + " - " + window.innerHeight + " = " + bottomSpace);
+		
         if (rightSpace > 0) {
-            $(contextMenu).css("left", $(contextMenu).position().left - rightSpace);
+            $(contextMenu).css("left", $(contextMenu).position().left - rightSpace - 40);
+        }
+        if (bottomSpace > 0) {
+            $(contextMenu).css("top", $(contextMenu).position().top - bottomSpace - 10);
         }
     }
+
     // Define resizeObserver and add it to every submenu on the page.
     const resizeObserver = new ResizeObserver((entries) => {
         for (let entry of entries) {
-            moveContextMenu(entry.contentRect.width);
+            moveContextMenu(entry.target);
         }
     });
-    for (let item of document.getElementsByClassName("submenu")) {
-        resizeObserver.observe(item);
+    for (let item of document.getElementsByClassName("submenu1")) {
+	    resizeObserver.observe(item);
+	} for (let item of document.getElementsByClassName("submenu2")) {
+	    resizeObserver.observe(item);
     }
 
     //Hide contextmenu:
