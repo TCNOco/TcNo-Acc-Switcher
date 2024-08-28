f08ac8abe7502ff56836a9569c2e803281af6cb6
Wesley Pyburn <10319195+TCNOco@users.noreply.github.com>
2024-08-28 21:24:24 +0200

    2024-08-28 - Fix for file uploading FTP

--- other/upload-to-ftp.js
+++ other/upload-to-ftp.js
@@ -45,18 +45,31 @@ export const uploadFile = async (sourcePath, targetPath, options) => {
 
 // Function to upload a directory recursively
 export const uploadDirectory = async (sourceDirectory, targetDirectory, options = {}) => {
+  console.log(`UPLOAD DIR: sourceDirectory [${sourceDirectory}] to targetDirectory [${targetDirectory}]`);
   options = {
     maxConcurrentUploads: 10,
     ...options,
   };
 
   if (options.cleanDestination) {
+    console.log(`Cleaning dest`);
     await deleteFile(targetDirectory, options).catch(() => {});
   }
 
+  console.log(`Starting uploads`);
   options.limit = options.limit || pLimit(options.maxConcurrentUploads);
 
-  const filePaths = await globby(`${sourceDirectory}/**/*`, { onlyFiles: true, absolute: true });
+  const absoluteSourceDirectory = `${path.resolve(sourceDirectory).replace(/\\/g, '/')}`;
+  console.log(`Absolute Source Directory: ${absoluteSourceDirectory}`);
+  const filePaths = await globby(`${absoluteSourceDirectory}/**/*`, { onlyFiles: true, absolute: true });
+  console.log(`Glob Pattern: ${absoluteSourceDirectory}/**/*`);
+  console.log(`Files found: ${filePaths.length}`);
+  
+  if (filePaths.length === 0) {
+    console.log('No files found to upload.');
+    return;
+  }
+
   await Promise.all(
     filePaths.map(async (sourcePath) => {
       const targetPath = path.join(targetDirectory, path.relative(sourceDirectory, sourcePath));
@@ -65,24 +78,39 @@ export const uploadDirectory = async (sourceDirectory, targetDirectory, options 
   );
 };
 
-// Start the upload process
-uploadDirectory("TcNo-Acc-Switcher-Client\\bin\\x64\\Release\\TcNo-Acc-Switcher",
-                "/Projects/AccSwitcher/latest-test",
-                options)
-  .then(() => console.log("All files and folders uploaded successfully."))
-  .catch(error => {
+async function uploadFiles() {
+  try {
+    await uploadDirectory(
+      "TcNo-Acc-Switcher-Client\\bin\\x64\\Release\\TcNo-Acc-Switcher",
+      "Projects/AccSwitcher/latest-test",
+      options
+    );
+    console.log("All files and folders uploaded successfully.");
+
+    // Upload Hashes
+    await uploadFile(
+      'TcNo-Acc-Switcher-Client\\bin\\x64\\Release\\UpdateOutput\\hashes.json',
+      'Projects/AccSwitcher/latest-test/hashes.json', 
+      options
+    );
+    console.log("hashes.json uploaded.");
+
+    await uploadFile(
+      'TcNo-Acc-Switcher-Client\\bin\\x64\\Release\\UpdateDiff.7z',
+      `Projects/AccSwitcher/updates-test/${process.env.DATEVERSION}.7z`,
+      options
+    );
+
+    await uploadFile(
+      'TcNo-Acc-Switcher-Client\\bin\\x64\\Release\\CEF.7z',
+      `Projects/AccSwitcher/updates-test/CEF.7z`,
+      options
+    );
+    console.log("UpdateDiff.7z uploaded.");
+  } catch (error) {
     console.error('Error during upload:', error);
     process.exit(1);
-  });
+  }
+}
 
-// Upload Hashes
-uploadFile('TcNo-Acc-Switcher-Client\\bin\\x64\\Release\\UpdateOutput\\hashes.json',
-            '/Projects/AccSwitcher/latest-test/hashes.json', options)
-  .then(() => console.log("hashes.json uploaded."))
-  .catch(error => {
-    console.error('Error during upload:', error);
-    process.exit(1);
-  });
-
-fs.copyFileSync('TcNo-Acc-Switcher-Client\\bin\\x64\\Release\\UpdateDiff.7z',
-                'UpdateDiff.7z');
+uploadFiles();
